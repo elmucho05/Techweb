2f14fb21f5c5ebfae5bb610c7629cf2a3757b4b1
Simo264 <simonevaiasinni@gmail.com>
2023-04-12 01:36:33 +0200

    to do: user model

---
+++ project/authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+  # http://127.0.0.1:8000/authentication/login
+  path("login/", views.login, name="login"),
+
+  # http://127.0.0.1:8000/authentication/signup
+  path("signup/", views.signup, name="signup"),
+]
--- project/movies/views.py
+++ project/movies/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
+  # search_query = request.GET.get("search", None)
   return render(request, "movies/index.html") 
