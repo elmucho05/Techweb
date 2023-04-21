8fd779e482852e2cf5d94efde221dcb6223bc2eb
Simo264 <simonevaiasinni@gmail.com>
2023-04-21 19:16:53 +0200

    to do profile page

--- project/user/urls.py
+++ project/user/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
   # http://127.0.0.1:8000/user/<uid>/profile/
   path('profile/', views.view_profile, name="view_profile"),
 
-  # http://127.0.0.1:8000/user/<uid>/set-avatar
+  # http://127.0.0.1:8000/user/<uid>/update-avatar
   path('update-avatar/', views.view_update_user_avatar, name="view_update_user_avatar"),
+
+    # http://127.0.0.1:8000/user/<uid>/update-password
+  path('update-password/', views.view_update_user_password, name="view_update_user_password"),
 ]
--- project/user/views.py
+++ project/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
+from django.contrib import messages
 
 from .models import User
 from .session import set_user_session_avatar_url
@@ -13,7 +14,10 @@ def view_profile(request, uid):
   except:
     return HttpResponseBadRequest(f"Invalid user")
   context = { 'user' : user }
+  
   return render(request, "user/profile.html", context)
+
+
 
 @require_http_methods(["POST"])
 def view_update_user_avatar(request, uid):
@@ -26,5 +30,15 @@ def view_update_user_avatar(request, uid):
   user.avatar = avatar
   user.save()
   set_user_session_avatar_url(request, str(user.avatar))
+  messages.success(request, "Immagine profilo aggiornata!")
   return redirect('view_profile', uid=uid)
+
+@require_http_methods(["POST"])
+def view_update_user_password(request, uid):
+  old_password = request.POST.get('old-password')
+  new_password = request.POST.get('new-password')
+
+  messages.error(request, "Password non valida!")
+  return redirect('view_profile', uid=uid)
+
 
